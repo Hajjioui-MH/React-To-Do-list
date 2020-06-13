1e6d6c922b779da070cb18517b999aca45135718
Hajjioui-MH <strongteethpro@gmail.com>
2020-06-13 12:51:05 +0100

    Updates

---
+++ precache-manifest.248b83214ffdecfd83f895a8f828c96d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0235d67910540fbef334ec3a29be7bfb",
+    "url": "/React-To-Do-list/index.html"
+  },
+  {
+    "revision": "e7dcd7414e1e63858b25",
+    "url": "/React-To-Do-list/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0ee22abafd133f0e9445",
+    "url": "/React-To-Do-list/static/css/main.ac7236d7.chunk.css"
+  },
+  {
+    "revision": "e7dcd7414e1e63858b25",
+    "url": "/React-To-Do-list/static/js/2.5b927f66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-list/static/js/2.5b927f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee22abafd133f0e9445",
+    "url": "/React-To-Do-list/static/js/main.93dd2ba3.chunk.js"
+  },
+  {
+    "revision": "fba81fecbb763560ca99",
+    "url": "/React-To-Do-list/static/js/runtime-main.d398bdb2.js"
+  }
+]);
